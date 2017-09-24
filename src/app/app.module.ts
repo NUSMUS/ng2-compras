9b466be6ec728fa8995b76516f1d2d2c47828852
Noé Hoyos Quiroz <devtomhet@gmail.com>
2017-09-24 16:14:59 -0500

    Implementacion del Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { ProveedoresService } from '../servicios/proveedores.service';
 
 import { AppComponent } from './app.component';
 import { ProveedoresComponent } from './proveedores/proveedores.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'proveedores', component: ProveedoresComponent },
+  { path: '**', component: InicioComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProveedoresComponent
+    ProveedoresComponent,
+    InicioComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     ProveedoresService
